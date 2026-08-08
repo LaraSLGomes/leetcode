8828038241135741873dbab6c6aaf2172b4cf376
LaraSLGomes <larastephanny05@gmail.com>
2026-08-07 23:36:14 -0300

    daily

--- 3302.py
+++ 3302.py
@@ -1,17 +1,18 @@
+class Solution:
     def validSequence(self, word1: str, word2: str) -> List[int]:
-        n, m = len(word1), len(word2)
-        last = [-1] * m
-        j = m - 1
-        for i in range(n - 1, -1, -1):
-            if j >= 0 and word1[i] == word2[j]:
-                last[j] = i
+        n = len(word2)
+        last = [-1]*n
+        j = n-1
+        for i, ch in reversed(list(enumerate(word1))): 
+            if j >= 0 and ch == word2[j]: 
+                last[j] = i 
                 j -= 1
-        res = []
-        skip = j = 0
-        for i, c in enumerate(word1):
-            if j == m: break
-            if word1[i] == word2[j] or skip == 0 and (j == m - 1 or i < last[j + 1]):
-                skip += word1[i] != word2[j]
-                res.append(i)
-                j += 1
-        return res if j == m else []
+        j = cnt = 0 
+        ans = []
+        for i, ch in enumerate(word1): 
+            if j < n: 
+                if ch == word2[j] or cnt == 0 and (j == n-1 or i+1 <= last[j+1]): 
+                    if ch != word2[j]: cnt = 1
+                    ans.append(i)
+                    j += 1
+        return ans if j == n else []
